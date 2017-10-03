9a5a463d2c4764bf606b2f48b4fc56f5ba59f0c3
Alexander Dibb <ajdibb1@gmail.com>
2017-10-03 01:15:25 -0700

    P! Deliverable - Menu Tweak

--- menu.py
+++ menu.py
@@ -1,9 +1,11 @@
 ################################################################################
-# sample_menu.py
+# menu.py
 # A. Hornof - Sept 2017
 #
 # A sample program to show how to move through a list of sound objects with
 # single keystrokes.
+# 
+# Edited by Alex Dibb - Oct 2017
 #
 ################################################################################
 __author__ = 'hornof'
@@ -30,13 +32,14 @@ def main():
 def create_sound_filenames():
 
     # Declare global variables.
-    global INTRO_WAV, YOU_SELECTED_WAV, NUMBERS_WAV, AM_WAV, PRESS_AGAIN_TO_QUIT_WAV,\
-        EXITING_PROGRAM_WAV, EXITING_PROGRAM_WAV_DURATION, TMP_FILE_WAV
+    global SET_DATE_AND_TIME_WAV, YOU_SELECTED_WAV, NUMBERS_WAV, AM_WAV,\
+        PRESS_AGAIN_TO_QUIT_WAV, EXITING_PROGRAM_WAV,\
+        EXITING_PROGRAM_WAV_DURATION, TMP_FILE_WAV
 
     # Create  sounds.
     nav_path = "wav_files_provided/miscellaneous_f/"
     num_path = "wav_files_provided/numbers_f/"
-    INTRO_WAV = nav_path + "Intro_f.wav"
+    SET_DATE_AND_TIME_WAV = nav_path + "Set_date_and_time_f.wav"
     YOU_SELECTED_WAV = nav_path + "you_selected_f.wav"
     NUMBERS_WAV = [num_path + "28_f.wav", num_path + "29_f.wav",
            num_path + "30_f.wav", num_path + "31_f.wav"]
@@ -61,7 +64,7 @@ def verify_sound_filenames():
     sound.Play(NUMBERS_WAV[2])
     sound.Play(NUMBERS_WAV[3])
     sound.Play(AM_WAV)
-    sound.Play(INTRO_WAV)
+    sound.Play(SET_DATE_AND_TIME_WAV)
 
 ################################################################################
 # Create some global constants and variables for the menu.
@@ -69,15 +72,23 @@ def verify_sound_filenames():
 def create_menu_globals():
 
     # Declare global variables as such.
-    global FORWARD_KEY, BACKWARD_KEY, QUIT_KEY, MINIMAL_HELP_STRING, CURRENT_TIME
+    global CONTINUE_KEY, FORWARD_KEY, BACKWARD_KEY, UNDO_KEY, QUIT_KEY,\
+        MINIMAL_HELP_STRING, CURRENT_TIME
 
     # Constants
     # Keystrokes for the keyboard interaction.
-    FORWARD_KEY = '\x20' # space bar
-    BACKWARD_KEY = ';'
-    QUIT_KEY = 'j'
+    CONTINUE_KEY = '\x20' # space bar
+    FORWARD_KEY = 'j'
+    BACKWARD_KEY = 'k'
+    UNDO_KEY = 'l'
+    QUIT_KEY = ';'
+
     # A bare minimum of text to display to guide the user.
-    MINIMAL_HELP_STRING = "Press '" + QUIT_KEY + "' to quit."
+    MINIMAL_HELP_STRING = "Press '" + FORWARD_KEY + "' and '" + BACKWARD_KEY \
+        + "' to go forward and back.\n" \
+        + "Press '" + CONTINUE_KEY + "' and '" + UNDO_KEY \
+        + "' to go continue and undo.\n" \
+        + "Press '" + QUIT_KEY + "' to quit."
 
     # Global variables
     CURRENT_TIME = 0    # The current time that is set. (Just an integer for now.)
@@ -109,7 +120,21 @@ def run_menu():
                 CURRENT_TIME = 0
 
             # Concatenate three audio files to generate the message.
-            sound.combine_wav_files(TMP_FILE_WAV, YOU_SELECTED_WAV,
+            sound.combine_wav_files(TMP_FILE_WAV, 
+                                    NUMBERS_WAV[CURRENT_TIME], AM_WAV)
+
+            # Play the concatenated file.
+            sound.Play(TMP_FILE_WAV)
+
+        if c == BACKWARD_KEY:
+
+            # Reduce the time, looping forward around to the end.
+            CURRENT_TIME -= 1
+            if CURRENT_TIME < 0:
+                CURRENT_TIME = len(NUMBERS_WAV) - 1
+
+            # Concatenate three audio files to generate the message.
+            sound.combine_wav_files(TMP_FILE_WAV,
                                     NUMBERS_WAV[CURRENT_TIME], AM_WAV)
 
             # Play the concatenated file.
